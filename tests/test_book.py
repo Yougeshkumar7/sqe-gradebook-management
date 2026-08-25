dfb270abff002d7bba0f9850f3acd68c3b20b8e3
Yougesh Kumar <katariayougesh@gmail.com>
2026-08-25 15:28:37 +0500

    test(book): add Book tests

---
+++ tests/test_book.py
@@ -0,0 +1,25 @@
+
+from src.book import Book
+
+
+def test_book_creation():
+    book = Book("Harry Potter", "J.K. Rowling")
+
+    assert book.title == "Harry Potter"
+    assert book.author == "J.K. Rowling"
+
+
+def test_empty_title_raises_error():
+    try:
+        Book("", "J.K. Rowling")
+        assert False
+    except ValueError:
+        assert True
+
+
+def test_empty_author_raises_error():
+    try:
+        Book("Harry Potter", "")
+        assert False
+    except ValueError:
+        assert True
